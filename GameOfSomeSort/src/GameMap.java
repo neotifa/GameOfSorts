ab07c1a07453e69911a1306cf15584fedc3ed98b
eboyer <boyer.373@osu.edu>
2014-10-04 13:15:13 -0400

    GameMap and Tile.

---
+++ GameOfSomeSort/src/GameMap.java
@@ -0,0 +1,42 @@
+/**
+ * Main game map structure. Might make it extendable for town maps and such.
+ * 
+ * @author eboyer
+ * 
+ */
+
+public class GameMap {
+
+    private static final Tile[][] map = new Tile[100][100];
+
+    /*
+     * Default constructor. No need for others.
+     */
+    private GameMap() {
+
+    }
+
+    private void init() {
+
+    }
+
+    private void drawWater() {
+
+    }
+
+    private void drawSand() {
+
+    }
+
+    private void drawGrass() {
+
+    }
+
+    private void drawBridge() {
+
+    }
+
+    private void drawMountain() {
+
+    }
+}
---
+++ GameOfSomeSort/src/Tile.java
@@ -0,0 +1,82 @@
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
+/**
+ * Class to handle the game tiles.
+ * 
+ * @author eboyer
+ * 
+ */
+public class Tile {
+
+    /**
+     * Default image for Tile.
+     */
+    private static final String DEFAULT_IMAGE = "images/default_tile.png";
+
+    private boolean traversable = false;
+    private BufferedImage img = null;
+
+    /**
+     * Default Constructor.
+     */
+    public Tile() {
+        this.setImage(DEFAULT_IMAGE);
+    }
+
+    /**
+     * Takes the name of a file for Tile image.
+     * 
+     * @param imgFile
+     *            File name for the image representation of this Tile.
+     * @requires imgFile != null & is valid file path
+     * 
+     */
+    public Tile(String imgFile) {
+        this.setImage(imgFile);
+    }
+
+    /**
+     * Sets the Tile's traversable value to TRUE or FALSE
+     */
+    public void setTraversable(boolean traversable) {
+        this.traversable = traversable;
+    }
+
+    /*
+     * Returns whether the Tile is traversable.
+     * 
+     * @return If this Tile is traversable
+     */
+    public boolean isTraversable() {
+        return this.traversable;
+    }
+
+    /*
+     * Sets the file path for the image.
+     * 
+     * @param imgFile File path for new image file.
+     */
+    public void setImage(String imgFile) {
+        try {
+            this.img = ImageIO.read(new File(imgFile));
+        } catch (IOException e) {
+            new JOptionPane(e.getMessage(), JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    /**
+     * Returns the Image object for the current Tile.
+     * 
+     * @return Current Image for this Tile.
+     */
+    public Image getImage() {
+        return this.img;
+    }
+
+}
