b6e7798644a017541b54048d41121371056c7eb8
eboyer <boyer.373@osu.edu>
2014-10-05 08:40:31 -0400

    Added Item back in?

---
+++ GameOfSomeSort/src/Item.java
@@ -0,0 +1,11 @@
+public class Item {
+
+    final static int DEFAULT_VALUE = 100;
+    final static int DEFAULT_SPACE = 1;
+    final static double DEFAULT_RARITY = 1.0;
+
+    private int value = this.DEFAULT_VALUE;
+    private int space = this.DEFAULT_SPACE;
+    private double rarity = this.DEFAULT_RARITY;
+
+}
